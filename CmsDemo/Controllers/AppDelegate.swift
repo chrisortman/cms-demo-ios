b1d46484d6a00172e5960dbf11d7bd21cd34bb7b
Chris Ortman <chris-ortman@uiowa.edu>
2016-11-30 11:02:52 -0600

    Initial Commit

---
+++ CmsDemo/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  CmsDemo
+//
+//  Created by Ortman on 11/30/16
+//  Copyright (c) 2016 chrisortman. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
